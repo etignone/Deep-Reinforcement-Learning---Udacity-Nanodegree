0cb25032cf7f74073c40cd2c74b902207d575a01
etignone <tignone@gmail.com>
2018-09-25 15:24:45 +0200

    Update dqn_agent.py

--- Udacity_Nanodegree/Navigation_Project/dqn_agent.py
+++ Udacity_Nanodegree/Navigation_Project/dqn_agent.py
@@ -10,7 +10,7 @@ from model import QNetwork
 
 #parameters
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 32         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
